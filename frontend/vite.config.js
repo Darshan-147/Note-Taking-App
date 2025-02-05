13f4e07e28684a55fcfdcd7ba85e37935d75ee1a
Darshan <darshaner2019@gmail.com>
2025-02-05 19:29:10 +0530

    config file changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Allow external access
+    allowedHosts: ["22k6wc-5173.csb.app"], // Add CodeSandbox URL
+  },
 });
